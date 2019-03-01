c088fed35ff5a70b0c39b958f588455f9fe29120
Gabriel Trompiz <gab.tc@icloud.com>
2019-03-01 12:59:14 -0400

    first implementation of express

---
+++ node/index.js
@@ -0,0 +1,9 @@
+const app = require('express')()
+const server = require('http').Server(app)
+const io = require('socket.io')
+
+server.listen(8080)
+
+app.get('/', (req, res) => {
+    res.send(200, 'BAROLCHI')
+})
