2d69e4f434b24f061eb9c1d3729e50c57e717b89
Gabriel Trompiz <gab.tc@icloud.com>
2019-03-03 16:00:48 -0400

    styling

--- react-app/src/views/Menu.js
+++ react-app/src/views/Menu.js
@@ -10,11 +10,14 @@ export default class Menu extends React.Component {
 
 	render() {
 		return(
-		<div id='menuCtn'>
-			<div id='titleContainer'><p id='title'>Battleship</p></div>
-			<button className='menuBtn' style={{marginTop: 20}}>Create Room</button><br></br>
-			<button className='menuBtn' onClick={() => this.props.changeView('RoomList')}>Join Room</button><br></br>
-			<button className='menuBtn'>Help</button><br></br>
-		</div>);
+			<div>
+				<div id='titleContainer'><p id='title'>SUPER<br />BATTLESHIP</p></div>
+				<div id='menuCtn'>
+					<button className='menuBtn' style={{marginTop: 20}}>Create Room</button><br></br>
+					<button className='menuBtn' onClick={() => this.props.changeView('RoomList')}>Join Room</button><br></br>
+					<button className='menuBtn'>Help</button><br></br>
+				</div>
+			</div>
+		);
 	}
 }
