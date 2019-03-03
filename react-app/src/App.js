2f351a4f415fa93f9b558aaea8f608023fa52bb1
Luis Petrella <aax_00ap@hotmail.com>
2019-03-03 03:21:35 -0400

    ok sigo siendo bagre
    Por lo menos tengo la iniciativa ps (Tranki mañana voy a ser la metralleta jonson)

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import Menu from './views/Menu';
 import RoomList from './views/RoomList'
+import Game from './views/Game';
 
 class App extends Component {
   render() {
     return (
       <div id='container'>
-          <RoomList/>
+          <Game/>
       </div>
     );
   }
---
+++ react-app/src/views/Game.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import '../styles/styles.css';
+
+export default class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return(
+          <div id='gameCtn'>
+              <div>
+                  <table>
+                      
+                  </table>
+              </div>
+
+              <div>
+                  <table>
+
+                  </table>
+              </div>
+
+              <div>
+
+              </div>
+
+              <div>
+
+              </div>
+          </div>
+        );
+    }
+}
